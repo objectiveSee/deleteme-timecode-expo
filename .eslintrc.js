24e82e587e881c826c576ad56e688a0efea9b732
Danny Ricciotti <dan.ricciotti@gmail.com>
2024-03-16 17:42:38 -0600

    ESLint setup

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "standard-with-typescript"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        "react/react-in-jsx-scope": "off",
+        "@typescript-eslint/triple-slash-reference": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+    }
+}
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,21 +1,21 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack } from 'expo-router'
+import { StyleSheet } from 'react-native'
 
-import { Text, View } from '@/components/Themed';
+import { Text, View } from '@/components/Themed'
 
-export default function NotFoundScreen() {
+export default function NotFoundScreen () {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn't exist.</Text>
+        <Text style={styles.title}>This screen doesn&apos;t exist.</Text>
 
         <Link href="/" style={styles.link}>
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
       </View>
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -23,18 +23,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
+    padding: 20
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   link: {
     marginTop: 15,
-    paddingVertical: 15,
+    paddingVertical: 15
   },
   linkText: {
     fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+    color: '#2e78b7'
+  }
+})
