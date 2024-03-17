80559bbaeff3f6b0a7e2c902443cb08a67616bdc
Danny Ricciotti <dan.ricciotti@gmail.com>
2024-03-16 19:35:24 -0600

    format time

--- src/components/NoteTextItem.tsx
+++ src/components/NoteTextItem.tsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import React, { useState } from 'react'
+import { Text, TextInput, View } from 'react-native'
 
-export const NoteTextItem = ({ text, date }: { text: string; date: string }) => {
-    const [noteText, setNoteText] = useState(text);
+export const NoteTextItem = ({ text, date }: { text: string, date: string }) => {
+  const [noteText, setNoteText] = useState(text)
 
-    return (
-        <View style={{ width: '100%' }}>
-            <Text style={{ backgroundColor: 'rgba(0, 0, 255, 0.1)', padding: 10 }}>
-                {date}
-            </Text>
-            <TextInput
-                style={{ width: '100%', backgroundColor: 'rgba(255, 0, 0, 0.1)', padding: 10 }}
-                multiline={true}
-                editable={true}
-                value={noteText}
-                onChangeText={setNoteText}
-                blurOnSubmit={true} // This should ensure the keyboard is dismissed when the return key is pressed in a multiline TextInput
-            />
-        </View>
-    );
+  return (
+    <View style={{ width: '100%' }}>
+      <Text style={{ backgroundColor: 'rgba(0, 0, 255, 0.1)', padding: 10 }}>
+        {date}
+      </Text>
+      <TextInput
+        style={{ width: '100%', backgroundColor: 'rgba(255, 0, 0, 0.1)', padding: 10 }}
+        multiline={true}
+        editable={true}
+        value={noteText}
+        onChangeText={setNoteText}
+        blurOnSubmit={true} // This should ensure the keyboard is dismissed when the return key is pressed in a multiline TextInput
+      />
+    </View>
+  )
 }
--- src/utilities/date.ts
+++ src/utilities/date.ts
@@ -1,10 +1,18 @@
-const FRAMES_PER_SECOND = 25
-
-export const formatTime = (timeMs: number) => {
-  const timeSeconds = timeMs / 1000
-  const hours = Math.floor(timeSeconds / 3600)
-  const minutes = Math.floor((timeSeconds % 3600) / 60)
-  const seconds = Math.floor(timeSeconds % 60)
-  const frames = Math.floor((timeSeconds % 1) * FRAMES_PER_SECOND)
-  return `${hours}:${minutes}:${seconds}:${frames}`
+export const formatTime = (time: number /* startingTime: number */) => {
+  // const diff = time - startingTime;
+  const diff = time
+  const hourDiff = diff / 1000 / 60 / 60
+  const hours = Math.floor(hourDiff)
+  const minuteDiff = (hourDiff - hours) * 60
+  const minutes = Math.floor(minuteDiff)
+  const secondDiff = (minuteDiff - minutes) * 60
+  const seconds = Math.floor(secondDiff)
+  const displayHours = hours % 24
+  // time in 10ths of a second
+  const subseconds = Math.floor((secondDiff - seconds) * 100)
+  return `${displayHours.toString().padStart(2, '0')}:${minutes
+    .toString()
+    .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${subseconds
+    .toString()
+    .padStart(2, '0')}`
 }
